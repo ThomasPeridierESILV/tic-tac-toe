bcfe2c9204b0d3dd29b0d962f02164d18300d993
Thomas Peridier <thomas.peridier@devinci.fr>
2015-11-17 14:47:38 +0100

    step 6

--- lib/app.jsx
+++ lib/app.jsx
@@ -11,18 +11,23 @@ var Box = React.createClass({
     return {value: this.props.initialValue};
   },
 
-  componentWillMount: function() {
+  /*componentWillMount: function() {
   	 var currentState = this;
 	  this.timer = setInterval(function(){
 		  	var oldValue = currentState.state.value;
 		  	var newValue = oldValue == 'X' ? 'O' : 'X';
 		  	currentState.setState({value: newValue});
 	  },1000);
-  },
+  },*/
+
+  handleClick: function() {
+  		var oldValue = this.state.value;
+    	this.setState({value: this.state.value = oldValue == 'X' ? 'O' : 'X'});
+  	},
 
   'render': function onRender () {
     return (
-      <button style={BoxStyle} >{this.state.value}</button>
+      <button style={BoxStyle} onClick={this.handleClick} >{this.state.value}</button>
     );
   }
 });
